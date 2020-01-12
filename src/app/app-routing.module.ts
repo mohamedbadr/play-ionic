4033db31185b62b2094710dd37db8dcdb043fef1
Mohamed Badr <eng.ma.badr@gmail.com>
2020-01-12 15:17:39 +0300

    added auth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { PlacesPage } from './places/places.page';
 import { DiscoverPageModule } from './places/discover/discover.module';
+import { AuthGuard } from './auth/auth.guard';
+import { AuthPageModule } from './auth/auth.module';
 
 const routes: Routes = [
   { path: '', redirectTo: 'places', pathMatch: 'full' },
   {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthPageModule)
+  },
+  {
     path: 'places',
-    loadChildren: () => import('./places/places.module').then(m => m.PlacesPageModule)
-  }
+    loadChildren: () => import('./places/places.module').then(m => m.PlacesPageModule),
+    canLoad: [AuthGuard]
+  },
+  // {
+  //   path: 'bookings',
+  //   loadChildren: () => import('./places/places.module').then(m => m.PlacesPageModule),
+  //   canLoad: [AuthGuard]
+  // }
 ];
 
 @NgModule({
